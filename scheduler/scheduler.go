40a325af09f016d32773130730f90b272c59a260
Krishnan V.R <krishnanvr@phonepe.com>
2016-04-17 10:48:47 +0000

    filename change

---
+++ scheduler/scheduler.go
@@ -0,0 +1,186 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package scheduler
+
+import (
+	"github.com/gogo/protobuf/proto"
+	"strconv"
+	//"time"
+//	"os"
+	"fmt"
+
+	log "github.com/golang/glog"
+	mesos "github.com/mesos/mesos-go/mesosproto"
+	util "github.com/mesos/mesos-go/mesosutil"
+	sched "github.com/mesos/mesos-go/scheduler"
+)
+
+type ExampleScheduler struct {
+	executor      *mesos.ExecutorInfo
+	tasksLaunched int
+	tasksFinished int
+	totalTasks    int
+	cpuPerTask    float64
+	memPerTask    float64
+	hostname      string
+	mac           string
+	comp_type     string
+}
+
+func NewExampleScheduler(exec *mesos.ExecutorInfo, taskCount int, cpuPerTask float64, memPerTask float64, hn *string, mac *string, comp_type *string) *ExampleScheduler {
+	return &ExampleScheduler{
+		executor:      exec,
+		tasksLaunched: 0,
+		tasksFinished: 0,
+		totalTasks:    taskCount,
+		cpuPerTask:    cpuPerTask,
+		memPerTask:    memPerTask,
+		hostname:      *hn,
+		mac:           *mac,
+		comp_type:     *comp_type,
+	}
+}
+
+func (sched *ExampleScheduler) Registered(driver sched.SchedulerDriver, frameworkId *mesos.FrameworkID, masterInfo *mesos.MasterInfo) {
+	log.Infoln("Scheduler Registered with Master ", masterInfo)
+}
+
+func (sched *ExampleScheduler) Reregistered(driver sched.SchedulerDriver, masterInfo *mesos.MasterInfo) {
+	log.Infoln("Scheduler Re-Registered with Master ", masterInfo)
+}
+
+func (sched *ExampleScheduler) Disconnected(sched.SchedulerDriver) {
+	log.Infoln("Scheduler Disconnected")
+}
+
+func (sched *ExampleScheduler) ResourceOffers(driver sched.SchedulerDriver, offers []*mesos.Offer) {
+	logOffers(offers)
+	if(sched.tasksLaunched > 0 ) {
+		fmt.Println("TASKS Already Launched - Returning") 
+		return
+	}
+	attrib_arbitary_high := 100
+	var chosen_offer *mesos.Offer
+
+	var tasks []*mesos.TaskInfo
+	for _, offer := range offers {
+		remainingCpus := getOfferCpu(offer)
+		remainingMems := getOfferMem(offer)
+
+		if sched.cpuPerTask <= remainingCpus && sched.memPerTask <= remainingMems {
+			get_attrib_for_offer := GetAttribVal(offer, sched.comp_type)
+			if get_attrib_for_offer < attrib_arbitary_high {
+				chosen_offer = offer
+			}
+		}
+	}
+
+	taskId := &mesos.TaskID{
+		Value: proto.String(strconv.Itoa(sched.tasksLaunched)),
+	}
+
+	task := &mesos.TaskInfo{
+		Name:     proto.String("kvm-(" + sched.hostname +")" + taskId.GetValue()),
+		TaskId:   taskId,
+		SlaveId:  chosen_offer.SlaveId,
+		Executor: sched.executor,
+		Resources: []*mesos.Resource{
+			util.NewScalarResource("cpus", sched.cpuPerTask),
+			util.NewScalarResource("mem", sched.memPerTask),
+		},
+	}
+	log.Infof("Prepared task: %s with offer %s for launch\n", task.GetName(), chosen_offer.Id.GetValue())
+
+	tasks = append(tasks, task)
+	log.Infoln("Launching ", len(tasks), "tasks for offer", chosen_offer.Id.GetValue())
+	driver.LaunchTasks([]*mesos.OfferID{chosen_offer.Id}, tasks, &mesos.Filters{RefuseSeconds: proto.Float64(1)})
+	sched.tasksLaunched++
+	return
+}
+
+/*
+func (sched *ExampleScheduler) ResourceOffers(driver sched.SchedulerDriver, offers []*mesos.Offer) {
+	logOffers(offers)
+	fmt.Printf("%+v\n",driver)
+	os.Exit(1)
+
+	for _, offer := range offers {
+		remainingCpus := getOfferCpu(offer)
+		remainingMems := getOfferMem(offer)
+
+		var tasks []*mesos.TaskInfo
+		if sched.cpuPerTask <= remainingCpus && sched.memPerTask <= remainingMems {
+
+			sched.tasksLaunched++
+
+			fmt.Println("TASKS LAUNCHED=",sched.tasksLaunched)
+			if(sched.tasksLaunched > sched.totalTasks) {
+				time.Sleep(100)
+				break
+			}
+
+			taskId := &mesos.TaskID{
+				Value: proto.String(strconv.Itoa(sched.tasksLaunched)),
+			}
+
+			task := &mesos.TaskInfo{
+				Name:     proto.String("kvm-"+taskId.GetValue()),
+				TaskId:   taskId,
+				SlaveId:  offer.SlaveId,
+				Executor: sched.executor,
+				Resources: []*mesos.Resource{
+					util.NewScalarResource("cpus", sched.cpuPerTask),
+					util.NewScalarResource("mem", sched.memPerTask),
+				},
+			}
+			log.Infof("Prepared task: %s with offer %s for launch\n", task.GetName(), offer.Id.GetValue())
+
+			tasks = append(tasks, task)
+			remainingCpus -= sched.cpuPerTask
+			remainingMems -= sched.memPerTask
+		}
+		log.Infoln("Launching ", len(tasks), "tasks for offer", offer.Id.GetValue())
+		driver.LaunchTasks([]*mesos.OfferID{offer.Id}, tasks, &mesos.Filters{RefuseSeconds: proto.Float64(1)})
+	}
+	return
+}
+*/
+func (sched *ExampleScheduler) StatusUpdate(driver sched.SchedulerDriver, status *mesos.TaskStatus) {
+	log.Infoln("Status update: task", status.TaskId.GetValue(), " is in state ", status.State.Enum().String())
+}
+
+func (sched *ExampleScheduler) OfferRescinded(s sched.SchedulerDriver, id *mesos.OfferID) {
+	log.Infof("Offer '%v' rescinded.\n", *id)
+}
+
+func (sched *ExampleScheduler) FrameworkMessage(s sched.SchedulerDriver, exId *mesos.ExecutorID, slvId *mesos.SlaveID, msg string) {
+	log.Infof("Received framework message from executor '%v' on slave '%v': %s.\n", *exId, *slvId, msg)
+}
+
+func (sched *ExampleScheduler) SlaveLost(s sched.SchedulerDriver, id *mesos.SlaveID) {
+	log.Infof("Slave '%v' lost.\n", *id)
+}
+
+func (sched *ExampleScheduler) ExecutorLost(s sched.SchedulerDriver, exId *mesos.ExecutorID, slvId *mesos.SlaveID, i int) {
+	log.Infof("Executor '%v' lost on slave '%v' with exit code: %v.\n", *exId, *slvId, i)
+}
+
+func (sched *ExampleScheduler) Error(driver sched.SchedulerDriver, err string) {
+	log.Infoln("Scheduler received error:", err)
+}
