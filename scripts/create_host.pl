441fdf6364609d26be5b3be1f5f44abaee95a19c
Krishnan V.R <krishnanvr@phonepe.com>
2016-05-17 08:22:03 +0000

    add baremetal

--- scripts/create_host.pl
+++ scripts/create_host.pl
@@ -10,7 +10,7 @@ use Getopt::Std ;
 
 my %qdata_opts; 
 
-getopts("Hh:c:v:q:s:e:", \%qdata_opts) ; 
+getopts("Hh:c:v:q:s:e:b:", \%qdata_opts) ; 
 
 die print_help() if(defined $qdata_opts{H}) ; 
 
@@ -21,11 +21,18 @@ my $vlan = $qdata_opts{v};
 my $q = $qdata_opts{q};
 my $size = $qdata_opts{s}; 
 my $exr = $qdata_opts{e}; 
+my $bm = $qdata_opts{b}; 
 
 
 die print_help()  if !(defined $vlan && defined $hostname && defined $ct && defined $q) ; 
 my $check_host_exists = PMCMesos::CheckDNS($hostname) ; 
 die if($check_host_exists) ; 
+if(defined $bm )  {
+	my @doms = split/\./,$bm ; 
+	if ( !(@doms > 3 && $doms[-1] =~ /^phonepe$/i) )  {
+		die "Baremetal should be name.phonepe.<dom>" 
+	}
+}
 my $sizef = PMCMesos::VerifyValidSize($size) ; 
 die "Invalid size : $size\n" if(!$sizef) ;
 my $host_ip = PMCMesos::GetFreeIP($vlan);
@@ -40,6 +47,8 @@ $qdata{cpu} = PMCMesos::GetCPU($size)  ;
 $qdata{mem} = PMCMesos::GetMemory($size) ;
 $qdata{comp_type} = $ct ;
 $qdata{executor} = $exr ;
+$qdata{baremetal} = $bm ;
+
 
 print Dumper \%qdata; 
 
@@ -52,6 +61,6 @@ sub print_help {
 	print "\n\t"; 
 	print __FILE__. " -H # This help \n"; 
 	print "\n\t"; 
-	print __FILE__. " -h <hostname> -c <component type> -v <vlan> -q <beanstalk end point> [-s C1M1024] [-e <executor> ]\n\n"; 
+	print __FILE__. " -h <hostname> -c <component type> -v <vlan> -q <beanstalk end point> [-s C1M1024] [-e <executor> ] [ -b baremetal IP ] \n\n"; 
 
 }
